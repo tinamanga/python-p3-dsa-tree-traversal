0c971c2377a35f4161706b943fcd09e4f5bfabc3
tinamanga <tinamanga@gmail.com>
2025-05-23 01:40:16 +0300

    Understand what tree traversal is Understand breadth-first vs. depth-first traversal Implement breadth-first and depth-first traversal algorithms Discuss Big O considerations of tree traversal algorithms Use tree traversal to recreate the getElementById method

--- lib/tree.py
+++ lib/tree.py
@@ -2,5 +2,14 @@ class Tree:
   def __init__(self, root = None):
     self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+  def get_element_by_id(self, id_to_find):
+        return self._dfs(self.root, id_to_find)
+
+  def _dfs(self, node, id_to_find):
+        if node['id'] == id_to_find:
+            return node
+        for child in node.get('children', []):
+            result = self._dfs(child, id_to_find)
+            if result:
+                return result
+        return None
